01131af8b9faeeee322b0bf8839565142e3c5085
Sanjoy <guinsanjoy34@gmail.com>
2026-01-30 22:37:35 +0530

    feat: add options & keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Custom keymaps
+
+vim.keymap.set("n", "<F9>", "<cmd>w<cr>", { desc = "Save file" })
+
+vim.keymap.set("n", "<leader>uw", function()
+  vim.wo.wrap = not vim.wo.wrap
+  vim.wo.linebreak = vim.wo.wrap
+  vim.wo.breakindent = vim.wo.wrap
+end, { desc = "Universal Word Wrap Toggle" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Always enable wrap globally
+vim.opt.wrap = true
+vim.opt.linebreak = true -- don't break words in the middle
+vim.opt.breakindent = true -- keep indentation on wrapped lines
+
+-- Force disable virtual text after LazyVim loads
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    vim.diagnostic.config({
+      virtual_text = false,
+      virtual_lines = false,
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+      severity_sort = true,
+    })
+  end,
+})
