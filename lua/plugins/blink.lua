567127990222b84e318897a5d6bcad9ed7973276
Sanjoy <guinsanjoy34@gmail.com>
2026-01-31 00:02:04 +0530

    fix: accept suggetions on Tab

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "select_and_accept" },
+        ["<S-Tab>"] = { "select_prev" },
+      },
+    },
+  },
+}
