ac66f6068fd93285fc2962dd4f55c2bc00e5470b
Sanjoy <guinsanjoy34@gmail.com>
2026-05-02 22:24:58 +0530

    fix: remove suggetions from .txt files

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,6 +2,9 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
+      enabled = function()
+        return vim.bo.filetype ~= "text"
+      end,
       keymap = {
         preset = "default",
         ["<Tab>"] = { "select_and_accept" },
