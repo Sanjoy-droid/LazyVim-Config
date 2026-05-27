f7b2035cf17f3691a0fadb1a713e95c1143b9d5b
Sanjoy <guinsanjoy34@gmail.com>
2026-05-28 00:13:52 +0530

    fix: add keymaps

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,46 +6,39 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
-  ---@module 'obsidian'
-  ---@type obsidian.config
+  keys = {
+    { "<leader>on", "<cmd>Obsidian new<cr>", desc = "New Note" },
+    { "<leader>oo", "<cmd>Obsidian quick_switch<cr>", desc = "Open Note" },
+    { "<leader>os", "<cmd>Obsidian search<cr>", desc = "Search Notes" },
+    { "<leader>od", "<cmd>Obsidian today<cr>", desc = "Daily Note" },
+  },
   opts = {
     legacy_commands = false,
-
     workspaces = {
       {
         name = "personal",
         path = "~/vaults/personal",
       },
     },
-
-    -- use telescope for all pickers
     picker = {
       name = "telescope.nvim",
     },
-
-    -- use blink.cmp for completion
     completion = {
       blink = true,
       nvim_cmp = false,
       min_chars = 2,
     },
-
-    -- daily notes config
     daily_notes = {
       folder = "dailies",
       date_format = "%Y-%m-%d",
       default_tags = { "daily" },
     },
-
-    -- where templates live inside the vault
     templates = {
       folder = "templates",
       date_format = "%Y-%m-%d",
       time_format = "%H:%M",
+      substitutions = {},
     },
-
-    -- when you hit <CR> on a link it follows it,
-    -- on a checkbox it toggles it
     mappings = {
       ["<CR>"] = {
         action = function()
@@ -54,12 +47,9 @@ return {
         opts = { buffer = true, expr = true },
       },
     },
-
-    -- open in vsplit instead of current window
     open_notes_in = "vsplit",
-
     ui = {
-      enable = true,
+      enabled = true,
     },
   },
 }
