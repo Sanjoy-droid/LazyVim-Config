d09bea3573d5dafd7026c67dda2cf91b8e2170bc
Sanjoy <guinsanjoy34@gmail.com>
2026-05-30 10:24:58 +0530

    fix: tailwind lsp detection in active client

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,36 @@
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  pattern = { "*.tsx", "*.ts", "*.jsx", "*.js", "*.css" },
+  callback = function()
+    local root = vim.fs.root(0, {
+      "postcss.config.mjs",
+      "postcss.config.js",
+      "tailwind.config.js",
+      "tailwind.config.ts",
+      "tailwind.config.mjs",
+    })
+    if root then
+      vim.lsp.start({
+        name = "tailwindcss",
+        cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/tailwindcss-language-server"), "--stdio" },
+        root_dir = root,
+        settings = {
+          tailwindCSS = {
+            experimental = { classRegex = {} },
+            files = {
+              exclude = { "**/.git/**", "**/node_modules/**", "**/.next/**", "**/dist/**" },
+            },
+          },
+        },
+      })
+    end
+  end,
+})
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
-      servers = {
-        tailwindcss = {
-          root_dir = require("lspconfig.util").root_pattern(
-            "tailwind.config.js",
-            "tailwind.config.ts",
-            "tailwind.config.mjs",
-            "postcss.config.js"
-          ),
-          settings = {
-            tailwindCSS = {
-              experimental = { classRegex = {} },
-              files = {
-                exclude = {
-                  "**/.git/**",
-                  "**/node_modules/**",
-                  "**/.next/**",
-                  "**/dist/**",
-                },
-              },
-            },
-          },
-        },
-      },
     },
   },
 }
