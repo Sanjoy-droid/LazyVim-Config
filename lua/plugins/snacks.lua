19a623358316ecaa8ce51fa14f0ac2a52a2c9e31
Sanjoy <guinsanjoy34@gmail.com>
2026-06-01 00:55:41 +0530

    fix: add snacks.lua

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,38 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    image = { enabled = true },
+    dashboard = { enabled = true },
+  },
+  keys = {
+    {
+      "<leader>gB",
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = "Git Browse",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit({ cwd = LazyVim.root() })
+      end,
+      desc = "Lazygit",
+    },
+    {
+      "<leader>un",
+      function()
+        Snacks.notifier.hide()
+      end,
+      desc = "Dismiss All Notifications",
+    },
+    {
+      "<c-/>",
+      function()
+        Snacks.terminal()
+      end,
+      desc = "Toggle Terminal",
+    },
+  },
+}
