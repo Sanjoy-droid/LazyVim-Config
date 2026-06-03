1c3084d715ec7aee77a54d4e917a7a30df6686c7
Sanjoy <guinsanjoy34@gmail.com>
2026-06-03 20:37:22 +0530

    fix: launching dir mismatch with project vs config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -18,42 +18,73 @@ return {
       },
     },
   },
-  keys = {
-    {
-      "<leader>gB",
-      function()
-        Snacks.gitbrowse()
-      end,
-      desc = "Git Browse",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>gg",
-      function()
-        Snacks.lazygit({ cwd = LazyVim.root() })
-      end,
-      desc = "Lazygit",
-    },
-    {
-      "<leader>un",
-      function()
-        Snacks.notifier.hide()
-      end,
-      desc = "Dismiss All Notifications",
-    },
-    {
-      "<c-/>",
-      function()
-        Snacks.terminal()
-      end,
-      desc = "Toggle Terminal",
-    },
-    {
-      "<c-_>",
-      function()
-        Snacks.terminal()
-      end,
-      desc = "which_key_ignore",
-    },
-  },
+  keys = (function()
+    local function smart_cwd()
+      local bufname = vim.api.nvim_buf_get_name(0)
+      if bufname ~= "" and vim.fn.filereadable(bufname) == 1 then
+        return LazyVim.root()
+      end
+      return vim.fn.getcwd()
+    end
+
+    return {
+      {
+        "<leader>gB",
+        function()
+          Snacks.gitbrowse()
+        end,
+        desc = "Git Browse",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>gg",
+        function()
+          Snacks.lazygit({ cwd = LazyVim.root() })
+        end,
+        desc = "Lazygit",
+      },
+      {
+        "<leader>un",
+        function()
+          Snacks.notifier.hide()
+        end,
+        desc = "Dismiss All Notifications",
+      },
+      {
+        "<c-/>",
+        function()
+          Snacks.terminal()
+        end,
+        desc = "Toggle Terminal",
+      },
+      {
+        "<c-_>",
+        function()
+          Snacks.terminal()
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<leader>e",
+        function()
+          Snacks.explorer({ cwd = smart_cwd() })
+        end,
+        desc = "Explorer (cwd)",
+      },
+      {
+        "<leader><space>",
+        function()
+          Snacks.picker.smart({ cwd = smart_cwd() })
+        end,
+        desc = "Smart Find Files",
+      },
+      {
+        "<leader>ff",
+        function()
+          Snacks.picker.files({ cwd = smart_cwd() })
+        end,
+        desc = "Find Files",
+      },
+    }
+  end)(),
 }
